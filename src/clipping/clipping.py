c74448832828429d10d0422df1bcdd84040d34f0
andreviniciusmb <mb.andrevinicius@gmail.com>
2022-10-29 14:07:16 -0300

    Clipping de reta liang-barsky adicionado

--- src/clipping/clipping.py
+++ src/clipping/clipping.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog, QGraphicsScene
 
 from clipping.poligonos import clipping_poligono
+from graficos import atualiza_lista_objetos, exibe_na_viewport
+from modelos.reta import Reta
 
 def verifica_clipping(ui: QDialog, 
                       scene: QGraphicsScene, 
@@ -10,10 +12,11 @@ def verifica_clipping(ui: QDialog,
     if ui.checkBox_ponto.isChecked():
         ## TODO Fazer clipping ponto
         pass
-    elif ui.checkBox_ponto.isChecked():
+    elif ui.checkBox_reta.isChecked():
         if ui.radioButton_liang.isChecked():
-            ## TODO Fazer clipping reta por Liang-Barsky
-            pass
+            for index in range(len(dados_saida)):
+                if isinstance(dados_saida[index], Reta):
+                    dados_saida[index].clipping_liang_barsky(dados_entrada= dados_entrada[0])
         elif ui.radioButton_cohen.isChecked():
             ## TODO Fazer clipping reta por Cohen -Sutherland
             pass
@@ -22,4 +25,11 @@ def verifica_clipping(ui: QDialog,
         clipping_poligono(ui= ui,
                           dados_entrada= dados_entrada,
                           dados_saida= dados_saida)
+
+    atualiza_lista_objetos(ui= ui,
+                           dados_saida= dados_saida)
+    exibe_na_viewport(ui= ui,
+                      scene= scene,
+                      dados_entrada=dados_entrada,
+                      dados_saida= dados_saida)
 
